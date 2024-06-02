5227cf7e375d9d90cf98460d1b7437e5ee0ac401
SaintBidies <144172427+SaintBid1es@users.noreply.github.com>
2024-06-02 15:24:36 +0300

    tut est kotiki
    tut est kotiki

---
+++ PracticeWPFBivlioteka/JsonConverterTest/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text.Json;
+
+namespace JsonConverterTest
+{
+
+    public class DataSerializer
+    {
+        public class Test
+        {
+            public string RandomTextOne { get; set; }
+            public string RandomTextTwo { get; set; }
+            public string RandomTextThree { get; set; }
+
+        }
+
+        public static void Serialize<T>(T data, string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                serializer.Serialize(fileStream, data);
+            }
+        }
+
+        public static T Deserialize<T>(string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                return (T)serializer.Deserialize(fileStream);
+            }
+        }
+    }
+}
+
+
+    
---
+++ PracticeWPFBivlioteka/PracticeWPFBivlioteka/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿
+using JsonConverterTest;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+using static JsonConverterTest.DataSerializer;
+namespace PracticeWPFBivlioteka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private  string filePath = "SUPER-PUPERFailExe.xml";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SerializedBtn1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var data = new Test
+                {
+                    RandomTextOne = NameTxb.Text,
+                    RandomTextTwo = SurNameTxb.Text,
+                    RandomTextThree = TelephoneTxb.Text
+
+                };
+                DataSerializer.Serialize(data, filePath);
+                MessageBox.Show("Ваши данные сохранены");
+                NameTxb.Text = string.Empty;
+                SurNameTxb.Text = string.Empty;
+                TelephoneTxb.Text = string.Empty;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void DeserializedBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    var data = DataSerializer.Deserialize<Test>(filePath);
+                    NameTxb.Text = data.RandomTextOne;
+                    SurNameTxb.Text = data.RandomTextTwo;
+                    TelephoneTxb.Text = data.RandomTextThree;
+                    MessageBox.Show("Ваши данные выгружены");
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+    }
+}
